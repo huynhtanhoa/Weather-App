5582ee298a9886edbe74e33077d8b403adb3e57f
Steven Huynh <huynhtanhoa2012@gmail.com>
2024-10-10 22:01:13 +0700

    complete handling errors of weather app

--- README.txt
+++ README.txt
@@ -3,3 +3,13 @@ npm init - y
 npm install request@2.88.0
 
 https://api.weatherstack.com/current?access_key=bd75edf5b57eb75b9a6c467ed00b2153&query=37.8267,-122.4233
+
+https://geocode.maps.co/search?q=Chau%20Doc&api_key=6707dff830ba2238989261jcnb701e7
+
+https://geocode.maps.co/search?q=Chau%20Doc&api_key=6707dff830ba2238989261jcnb701e7
+
+Forward Geocode (Convert human-readable address to coordinates):
+https://geocode.maps.co/search?q=address&api_key=6707dff830ba2238989261jcnb701e7
+
+Reverse Geocode (Convert coordinates to human-readable address):
+https://geocode.maps.co/reverse?lat=latitude&lon=longitude&api_key=6707dff830ba2238989261jcnb701e7
--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const request = require('request');
 
 const url = 'https://api.weatherstack.com/current?access_key=bd75edf5b57eb75b9a6c467ed00b2153&query=37.8267,-122.4233';
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current);
+request({ url: url, json:true}, (error, response) => {
+    if(error){
+        console.log("Unable to access to weather service")
+    } else if(response.body.error) {
+        console.log("Unable to find the location")
+    }
+    else {
+        console.log("description: " + response.body.current.weather_descriptions[0]);
+        console.log("temperature: " + response.body.current.temperature);
+        console.log("feelslike: " + response.body.current.feelslike);
+    }
 })
 
-console.log("Stopping");
+// Geocoding:
+const geoUrl = 'https://geocode.maps.co/search?q=Los%20Angeles&api_key=6707dff830ba2238989261jcnb701e7&';
+
+request({url: geoUrl, json:true}, (error, response) => {
+    if(error) {
+        console.log("Unable to access the Geocoding service")
+    } else if(response.body.length === 0){
+        console.log("Wrong location")
+    } else {
+        console.log("lat: " + response.body[0].lat)
+        console.log("lon: " + response.body[0].lon)
+    }
+})
